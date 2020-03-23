901cef065843975d56fc49f14b37375a04b971ad
Lucas Nascimento <lucas@n3xt.com.br>
2020-03-23 14:18:31 -0300

    Add horizontal progress bar

--- docs/js/main.js
+++ docs/js/main.js
@@ -183,11 +183,11 @@ try {
   for (var _iterator = progressBars[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
     var _progressBar = _step.value;
 
-    if (_progressBar.classList.contains("progress-bar--sm")) {
+    if (_progressBar.classList.contains("progress-bar--sm") && _progressBar.querySelector('circle')) {
       _progressBar.querySelector('circle').r.baseVal.value = "9";
-    } else if (_progressBar.classList.contains("progress-bar--md")) {
+    } else if (_progressBar.classList.contains("progress-bar--md") && _progressBar.querySelector('circle')) {
       _progressBar.querySelector('circle').r.baseVal.value = "11";
-    } else if (_progressBar.classList.contains("progress-bar")) {
+    } else if (_progressBar.classList.contains("progress-bar") && _progressBar.querySelector('circle')) {
       _progressBar.querySelector('circle').r.baseVal.value = "24";
     }
   } // Set default percentage for progress bar examples large, medium and small
@@ -220,46 +220,69 @@ progressBar({
   'id': 'example-progress-bar-sm'
 });
 progressBar({
-  'id': 'example-progress-bar-live'
+  'id': 'live-example-c'
 });
-var input = document.querySelector('#example-progress-value');
-input.addEventListener('change', function (e) {
-  if (input.value < 101 && input.value > -1) {
-    progressBar({
-      'percent': input.value,
-      'id': 'example-progress-bar-live'
-    });
-  }
+progressBar({
+  'id': 'live-example-h',
+  'type': 'horizontal'
+}); // Live example circle progress bar
+
+var inputCircle = document.querySelector('#live-example-value-c');
+inputCircle.addEventListener('change', function (e) {
+  progressBar({
+    'percent': inputCircle.value,
+    'id': 'live-example-c'
+  });
+}); // Live example horizontal progress bar
+
+var inputHorizontal = document.querySelector('#live-example-value-h');
+inputHorizontal.addEventListener('change', function (e) {
+  progressBar({
+    'percent': inputHorizontal.value,
+    'id': 'live-example-h',
+    'type': 'horizontal'
+  });
 });
 /**
  * Progress bar function
  * Parameters:
- * INT percent    = percent value
+ * INT percent = percent value
  * STRING id = id of id
+ * STRING type = type of progress bar ('circle' or 'horizontal')
 */
 
 function progressBar(params) {
   if (!params.id) return false;
   var elementId = "#" + params.id;
   var percent = params.percent ? params.percent : 0;
-  var circle = document.querySelector(elementId).querySelector('circle');
-  var text = document.querySelector(elementId).querySelector('.progress-bar__text');
-  var radius = circle.r.baseVal.value;
-  var circumference = radius * 2 * Math.PI;
-  circle.style.strokeDasharray = "".concat(circumference, " ").concat(circumference);
-  circle.style.strokeDashoffset = "".concat(circumference);
-  var offset = circumference - percent / 100 * circumference;
-  circle.style.strokeDashoffset = offset; // Change percentage text
+  var type = params.type ? params.type : 'circle';
 
-  if (text) {
-    text.innerHTML = percent + "%";
-  } // Change element color when percent is complete
+  if (type == 'circle') {
+    // Circle attributes
+    var text = document.querySelector(elementId).querySelector('.progress-bar__text');
+    var circle = document.querySelector(elementId).querySelector('circle');
+    var radius = circle.r.baseVal.value;
+    var circumference = radius * 2 * Math.PI; // Defines the default size of green and gray circle around the icon
+
+    circle.style.strokeDasharray = "".concat(circumference, " ").concat(circumference);
+    circle.style.strokeDashoffset = "".concat(circumference); // Calculate the green circle size according to the percentage entered
+
+    var offset = circumference - percent / 100 * circumference;
+    circle.style.strokeDashoffset = offset; // Change percentage text
+
+    if (text) {
+      text.innerHTML = percent + "%";
+    } // Change element color when percent is 100
 
 
-  if (percent == 100) {
-    document.querySelector(elementId).classList.add("progress-bar--done");
+    if (percent == 100) {
+      document.querySelector(elementId).classList.add("progress-bar--done");
+    } else {
+      document.querySelector(elementId).classList.remove("progress-bar--done");
+    }
   } else {
-    document.querySelector(elementId).classList.remove("progress-bar--done");
+    // Set percent of horizontal progress bar
+    document.querySelector(elementId).value = percent;
   }
 }
 
--- js/components/progressbar.js
+++ js/components/progressbar.js
@@ -2,66 +2,96 @@
 // Set default value r for circle in svg
 let progressBars = document.querySelectorAll('.progress-bar');
 for(let progressBar of progressBars){
-  if(progressBar.classList.contains("progress-bar--sm")){
-    progressBar.querySelector('circle').r.baseVal.value = "9";
-  } else if(progressBar.classList.contains("progress-bar--md")){
-    progressBar.querySelector('circle').r.baseVal.value = "11";
-  } else if(progressBar.classList.contains("progress-bar")){
-    progressBar.querySelector('circle').r.baseVal.value = "24";
-  }
+    if(progressBar.classList.contains("progress-bar--sm") && progressBar.querySelector('circle')){
+        progressBar.querySelector('circle').r.baseVal.value = "9";
+    } else if(progressBar.classList.contains("progress-bar--md") && progressBar.querySelector('circle')){
+        progressBar.querySelector('circle').r.baseVal.value = "11";
+    } else if(progressBar.classList.contains("progress-bar") && progressBar.querySelector('circle')){
+        progressBar.querySelector('circle').r.baseVal.value = "24";
+    }
 }
 
 // Set default percentage for progress bar examples large, medium and small
 progressBar({'percent':50, 'id':'example-progress-bar'});
 progressBar({'percent':50, 'id':'example-progress-bar-md'});
 progressBar({'percent':50, 'id':'example-progress-bar-sm'});
-progressBar({'id':'example-progress-bar-live'});
+progressBar({'id':'live-example-c'});
+progressBar({'id':'live-example-h', 'type':'horizontal'});
 
-const input = document.querySelector('#example-progress-value');
-input.addEventListener('change', function(e) 
-{
-  if (input.value < 101 && input.value > -1) {
-    progressBar({'percent':input.value, 'id':'example-progress-bar-live'});
-  }  
-})
+// Live example circle progress bar
+let inputCircle = document.querySelector('#live-example-value-c');
+inputCircle
+    .addEventListener('change', function(e) 
+    {
+        progressBar({
+            'percent':inputCircle.value,
+            'id':'live-example-c'
+        });
+    })
+
+// Live example horizontal progress bar
+let inputHorizontal = document.querySelector('#live-example-value-h');
+inputHorizontal
+    .addEventListener('change', function(e) 
+    {
+        progressBar({
+            'percent':inputHorizontal.value,
+            'id':'live-example-h',
+            'type':'horizontal'
+        });
+    })
 
 /**
  * Progress bar function
  * Parameters:
- * INT percent    = percent value
+ * INT percent = percent value
  * STRING id = id of id
+ * STRING type = type of progress bar ('circle' or 'horizontal')
 */
 function progressBar(params)
 {
-  
-  if(!params.id) return false;
+    if(!params.id) return false;
 
-  var elementId = "#"+params.id;
-  var percent = params.percent?params.percent:0;
-  var text = document.querySelector(elementId).querySelector('.progress-bar__text');
+    var elementId = "#"+params.id;
+    var percent = params.percent?params.percent:0;
+    var type = params.type?params.type:'circle';
 
-  // Circle attributes
-  var circle = document.querySelector(elementId).querySelector('circle');
-  var radius = circle.r.baseVal.value;
-  var circumference = radius * 2 * Math.PI;
-  
-  // Defines the default size of green and gray circle around the icon
-  circle.style.strokeDasharray = `${circumference} ${circumference}`;
-  circle.style.strokeDashoffset = `${circumference}`;
+    if(type == 'circle') {
+        // Circle attributes
+        var text = document.querySelector(elementId).querySelector('.progress-bar__text');
+        var circle = document.querySelector(elementId).querySelector('circle');
+        var radius = circle.r.baseVal.value;
+        var circumference = radius * 2 * Math.PI;
 
-  // Defines the green circle size according to the percentage entered
-  const offset = circumference - percent / 100 * circumference;
-  circle.style.strokeDashoffset = offset;
-  
-  // Change percentage text
-  if(text){
-    text.innerHTML = percent+"%";
-  }
+        // Defines the default size of green and gray circle around the icon
+        circle.style.strokeDasharray = `${circumference} ${circumference}`;
+        circle.style.strokeDashoffset = `${circumference}`;
 
-  // Change element color when percent is 100
-  if(percent == 100) {
-    document.querySelector(elementId).classList.add("progress-bar--done");
-  } else {
-    document.querySelector(elementId).classList.remove("progress-bar--done");
-  }
+        // Calculate the green circle size according to the percentage entered
+        const offset = circumference - percent / 100 * circumference;
+        circle.style.strokeDashoffset = offset;
+
+        // Change percentage text
+        if(text) {
+            text.innerHTML = percent+"%";
+        }
+
+        // Change element color when percent is 100
+        if(percent == 100) {
+            document
+                .querySelector(elementId)
+                .classList
+                .add("progress-bar--done");
+        } else {
+            document
+                .querySelector(elementId)
+                .classList
+                .remove("progress-bar--done");
+        }
+    } else {
+        // Set percent of horizontal progress bar
+        document
+            .querySelector(elementId)
+            .value = percent;
+    }
 }
