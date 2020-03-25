8d03ca39476e4090056df9aeb55741e3d4bb40e1
Lucas Nascimento <lucas@n3xt.com.br>
2020-03-25 16:10:48 -0300

    Fix progress bar

--- docs/dist/js/main.js
+++ docs/dist/js/main.js
@@ -52,11 +52,129 @@ if (iconsPlacement) {
 },{}],2:[function(require,module,exports){
 "use strict";
 
-require('../../../js/main');
+// Set default percentage for progress bar examples large, medium and small
+progressBar({
+  'percent': 50,
+  'id': 'example-progress-bar'
+});
+progressBar({
+  'percent': 50,
+  'id': 'example-progress-bar-md'
+});
+progressBar({
+  'percent': 50,
+  'id': 'example-progress-bar-sm'
+});
+progressBar({
+  'id': 'live-example-c'
+});
+progressBar({
+  'id': 'live-example-h',
+  'type': 'horizontal'
+}); // Live example circle progress bar
+
+var inputCircle = document.querySelector('#live-example-value-c');
+inputCircle.addEventListener('change', function (e) {
+  progressBar({
+    'percent': inputCircle.value,
+    'id': 'live-example-c'
+  });
+}); // Live example horizontal progress bar
+
+var inputHorizontal = document.querySelector('#live-example-value-h');
+inputHorizontal.addEventListener('change', function (e) {
+  progressBar({
+    'percent': inputHorizontal.value,
+    'id': 'live-example-h',
+    'type': 'horizontal'
+  });
+}); // Set default value r for circle in svg
+
+var progressBars = document.querySelectorAll('.progress-bar');
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = progressBars[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var _progressBar = _step.value;
+
+    if (_progressBar.classList.contains("progress-bar--sm") && _progressBar.querySelector('circle')) {
+      _progressBar.querySelector('circle').r.baseVal.value = "9";
+    } else if (_progressBar.classList.contains("progress-bar--md") && _progressBar.querySelector('circle')) {
+      _progressBar.querySelector('circle').r.baseVal.value = "11";
+    } else if (_progressBar.classList.contains("progress-bar") && _progressBar.querySelector('circle')) {
+      _progressBar.querySelector('circle').r.baseVal.value = "24";
+    }
+  }
+  /**
+   * Progress bar function
+   * Parameters:
+   * INT percent = percent value
+   * STRING id = id of element
+   * STRING type = type of progress bar ('circle' or 'horizontal')
+  */
+
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+      _iterator["return"]();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+function progressBar(params) {
+  if (!params.id) return false;
+  var elementId = "#" + params.id;
+  var percent = params.percent ? params.percent : 0;
+  var type = params.type ? params.type : 'circle';
+
+  if (type == 'circle') {
+    // Circle attributes
+    var text = document.querySelector(elementId).querySelector('.progress-bar__text');
+    var circle = document.querySelector(elementId).querySelector('circle');
+    var radius = circle.r.baseVal.value;
+    var circumference = radius * 2 * Math.PI; // Defines the default size of green and gray circle around the icon
+
+    circle.style.strokeDasharray = "".concat(circumference, " ").concat(circumference);
+    circle.style.strokeDashoffset = "".concat(circumference); // Calculate the green circle size according to the percentage entered
+
+    var offset = circumference - percent / 100 * circumference;
+    circle.style.strokeDashoffset = offset; // Change percentage text
+
+    if (text) {
+      text.innerHTML = percent + "%";
+    } // Change element color when percent is 100
+
+
+    if (percent == 100) {
+      document.querySelector(elementId).classList.add("progress-bar--done");
+    } else {
+      document.querySelector(elementId).classList.remove("progress-bar--done");
+    }
+  } else {
+    // Set percent of horizontal progress bar
+    document.querySelector(elementId).value = percent;
+  }
+}
+
+},{}],3:[function(require,module,exports){
+"use strict";
+
+require('../../js/main');
 
 require('./components/icons');
 
-},{"../../js/main":6,"./components/icons":1}],3:[function(require,module,exports){
+require('./components/progressbar');
+
+},{"../../js/main":7,"./components/icons":1,"./components/progressbar":2}],4:[function(require,module,exports){
 "use strict";
 
 /**
@@ -94,7 +212,7 @@ try {
   }
 }
 
-},{}],4:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 "use strict";
 
 /**
@@ -170,7 +288,7 @@ try {
   }
 }
 
-},{}],5:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 "use strict";
 
 // Set default value r for circle in svg
@@ -190,7 +308,14 @@ try {
     } else if (_progressBar.classList.contains("progress-bar") && _progressBar.querySelector('circle')) {
       _progressBar.querySelector('circle').r.baseVal.value = "24";
     }
-  } // Set default percentage for progress bar examples large, medium and small
+  }
+  /**
+   * Progress bar function
+   * Parameters:
+   * INT percent = percent value
+   * STRING id = id of element
+   * STRING type = type of progress bar ('circle' or 'horizontal')
+  */
 
 } catch (err) {
   _didIteratorError = true;
@@ -206,50 +331,6 @@ try {
     }
   }
 }
-
-progressBar({
-  'percent': 50,
-  'id': 'example-progress-bar'
-});
-progressBar({
-  'percent': 50,
-  'id': 'example-progress-bar-md'
-});
-progressBar({
-  'percent': 50,
-  'id': 'example-progress-bar-sm'
-});
-progressBar({
-  'id': 'live-example-c'
-});
-progressBar({
-  'id': 'live-example-h',
-  'type': 'horizontal'
-}); // Live example circle progress bar
-
-var inputCircle = document.querySelector('#live-example-value-c');
-inputCircle.addEventListener('change', function (e) {
-  progressBar({
-    'percent': inputCircle.value,
-    'id': 'live-example-c'
-  });
-}); // Live example horizontal progress bar
-
-var inputHorizontal = document.querySelector('#live-example-value-h');
-inputHorizontal.addEventListener('change', function (e) {
-  progressBar({
-    'percent': inputHorizontal.value,
-    'id': 'live-example-h',
-    'type': 'horizontal'
-  });
-});
-/**
- * Progress bar function
- * Parameters:
- * INT percent = percent value
- * STRING id = id of id
- * STRING type = type of progress bar ('circle' or 'horizontal')
-*/
 
 function progressBar(params) {
   if (!params.id) return false;
@@ -286,7 +367,7 @@ function progressBar(params) {
   }
 }
 
-},{}],6:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 "use strict";
 
 require('./components/alert');
@@ -298,6 +379,6 @@ require('./components/progressbar');
 module.exports = {// Your module right here
 };
 
-},{"./components/alert":3,"./components/navbar":4,"./components/progressbar":5}]},{},[2]);
+},{"./components/alert":4,"./components/navbar":5,"./components/progressbar":6}]},{},[3]);
 
 //# sourceMappingURL=main.js.map
