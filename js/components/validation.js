fe74966b8bbabd410f42da52e3ef0e20f9efbaa0
Lucas Nascimento <lucas@n3xt.com.br>
2020-04-03 12:24:03 -0300

    Use form validation only in forms with class form-validation

--- js/components/validation.js
+++ js/components/validation.js
@@ -1,8 +1,13 @@
-document.getElementsByTagName('form')[0].addEventListener("submit", function (event) {
-    // Each time the user tries to send the data, we check all inputs
-    for(let input of document.getElementsByTagName('form')[0].getElementsByTagName("input")){
-        if (!input.validity.valid) {
-            input.parentElement.querySelector('.form-error').classList.add("form-error--active");
+var forms = document.querySelectorAll('.form-validation');
+
+for(let form of forms){
+    form.addEventListener("submit", function (event) {
+        
+        // Each time the user tries to send the data, we check all inputs
+        for(let input of form.getElementsByTagName("input")){
+            if (!input.validity.valid) {
+                input.parentElement.querySelector('.form-error').classList.add("form-error--active");
+            }
         }
-    }
-}, false);
+    }, false);
+}
