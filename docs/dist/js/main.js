95ff5973646a76536cc5db90eeca2ff332c86dce
Lucas Nascimento <lucas@n3xt.com.br>
2020-05-07 19:02:26 -0300

    Adjust some variables name

--- docs/dist/js/main.js
+++ docs/dist/js/main.js
@@ -140,99 +140,105 @@ for (var i = 0; i < actions.length; i++) {
 },{}],5:[function(require,module,exports){
 "use strict";
 
-var x, i, j, selElmnt, a, b, c, selOption;
-/* Look for any elements with the class "custom-select" */
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    setTimeout(function () {
+      var select_div_container, i, j, select_element, select_div, options_div, option_div;
+      /* Look for any elements with the class "custom-select" */
 
-x = document.getElementsByClassName("custom-select");
+      select_div_container = document.getElementsByClassName("custom-select");
 
-for (i = 0; i < x.length; i++) {
-  if (x[i].getElementsByTagName("select")) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    /* For each element, create a new DIV that will act as the selected item */
+      for (i = 0; i < select_div_container.length; i++) {
+        if (select_div_container[i].getElementsByTagName("select")) {
+          select_element = select_div_container[i].getElementsByTagName("select")[0];
+          /* For each element, create a new DIV that will act as the selected item */
 
-    a = document.createElement("DIV");
-    a.setAttribute("class", selElmnt.disabled ? "custom-select__selected custom-select__selected--disabled" : "custom-select__selected");
-    selOption = selElmnt.selectedIndex > 0 ? selElmnt.selectedIndex : 0;
-    a.innerHTML = selElmnt.options[selOption].innerHTML;
-    x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list */
+          select_div = document.createElement("DIV");
+          select_div.setAttribute("class", select_element.disabled ? "custom-select__selected custom-select__selected--disabled" : "custom-select__selected");
+          select_element.selectedIndex = select_element.selectedIndex > 0 ? select_element.selectedIndex : 0;
+          select_div.innerHTML = select_element.options[select_element.selectedIndex].innerHTML;
+          select_div_container[i].appendChild(select_div);
+          /* For each element, create a new DIV that will contain the option list */
 
-    b = document.createElement("DIV");
-    b.setAttribute("class", "custom-select__items custom-select__items--hidden");
+          options_div = document.createElement("DIV");
+          options_div.setAttribute("class", "custom-select__items custom-select__items--hidden");
 
-    for (j = 1; j < selElmnt.length; j++) {
-      /* For each option in the original select element,
-      create a new DIV that will act as an option item */
-      c = document.createElement("DIV");
-      c.innerHTML = selElmnt.options[j].innerHTML;
-      c.addEventListener("click", function (e) {
-        /* When an item is clicked, update the original select box,
-        and the selected item */
-        var y, i, k, s, h;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        h = this.parentNode.previousSibling;
+          for (j = 1; j < select_element.length; j++) {
+            /* For each option in the original select element,
+            create a new DIV that will act as an option item */
+            option_div = document.createElement("DIV");
+            option_div.innerHTML = select_element.options[j].innerHTML;
+            option_div.addEventListener("click", function (e) {
+              /* When an item is clicked, update the original select box,
+              and the selected item */
+              var y, i, k, s, h;
+              s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+              h = this.parentNode.previousSibling;
 
-        for (i = 0; i < s.length; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("custom-select__items--selected");
+              for (i = 0; i < s.length; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                  s.selectedIndex = i;
+                  h.innerHTML = this.innerHTML;
+                  y = this.parentNode.getElementsByClassName("custom-select__items--selected");
 
-            for (k = 0; k < y.length; k++) {
-              y[k].removeAttribute("class");
-            }
+                  for (k = 0; k < y.length; k++) {
+                    y[k].removeAttribute("class");
+                  }
 
-            this.setAttribute("class", "custom-select__items--selected");
-            break;
+                  this.setAttribute("class", "custom-select__items--selected");
+                  break;
+                }
+              }
+
+              h.click();
+            });
+            options_div.appendChild(option_div);
+          }
+
+          select_div_container[i].appendChild(options_div);
+          select_div.addEventListener("click", function (e) {
+            /* When the select box is clicked, close any other select boxes,
+            and open/close the current select box if not disabled */
+            e.stopPropagation();
+            closeAllSelect(this);
+            this.nextSibling.classList.toggle("custom-select__items--hidden");
+            this.classList.toggle("custom-select__selected--active");
+          });
+        }
+      }
+
+      function closeAllSelect(elmnt) {
+        /* A function that will close all select boxes in the document,
+        except the current select box */
+        var options_div,
+            option_selected,
+            i,
+            arrNo = [];
+        options_div = document.getElementsByClassName("custom-select__items");
+        option_selected = document.getElementsByClassName("custom-select__selected");
+
+        for (i = 0; i < option_selected.length; i++) {
+          if (elmnt == option_selected[i]) {
+            arrNo.push(i);
+          } else {
+            option_selected[i].classList.remove("custom-select__selected--active");
           }
         }
 
-        h.click();
-      });
-      b.appendChild(c);
-    }
+        for (i = 0; i < options_div.length; i++) {
+          if (arrNo.indexOf(i)) {
+            options_div[i].classList.add("custom-select__items--hidden");
+          }
+        }
+      }
+      /* If the user clicks anywhere outside the select box,
+      then close all select boxes: */
 
-    x[i].appendChild(b);
-    a.addEventListener("click", function (e) {
-      /* When the select box is clicked, close any other select boxes,
-      and open/close the current select box if not disabled */
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("custom-select__items--hidden");
-      this.classList.toggle("custom-select__selected--active");
-    });
+
+      document.addEventListener("click", closeAllSelect);
+    }, 1000);
   }
-}
-
-function closeAllSelect(elmnt) {
-  /* A function that will close all select boxes in the document,
-  except the current select box */
-  var x,
-      y,
-      i,
-      arrNo = [];
-  x = document.getElementsByClassName("custom-select__items");
-  y = document.getElementsByClassName("custom-select__selected");
-
-  for (i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i);
-    } else {
-      y[i].classList.remove("custom-select__selected--active");
-    }
-  }
-
-  for (i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("custom-select__items--hidden");
-    }
-  }
-}
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-
-
-document.addEventListener("click", closeAllSelect);
+};
 
 },{}],6:[function(require,module,exports){
 "use strict";
--- js/components/custom-select.js
+++ js/components/custom-select.js
@@ -1,86 +1,99 @@
-var x, i, j, selElmnt, a, b, c, selOption;
-/* Look for any elements with the class "custom-select" */
-x = document.getElementsByClassName("custom-select");
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    setTimeout(function(){
+      var select_div_container, i, j, select_element, select_div, options_div, option_div;
 
-for (i = 0; i < x.length; i++) {
-  if(x[i].getElementsByTagName("select")){
-    selElmnt = x[i].getElementsByTagName("select")[0];
+      /* Look for any elements with the class "custom-select" */
+      select_div_container = document.getElementsByClassName("custom-select");
 
-    /* For each element, create a new DIV that will act as the selected item */
-    a = document.createElement("DIV");
-    a.setAttribute("class", (selElmnt.disabled?"custom-select__selected custom-select__selected--disabled":"custom-select__selected"));
-    selOption = selElmnt.selectedIndex > 0?selElmnt.selectedIndex:0;
-    a.innerHTML = selElmnt.options[selOption].innerHTML;
-    x[i].appendChild(a);
+      for (i = 0; i < select_div_container.length; i++) {
+        if(select_div_container[i].getElementsByTagName("select")){
+          select_element = select_div_container[i].getElementsByTagName("select")[0];
+  
+          /* For each element, create a new DIV that will act as the selected item */
+          select_div = document.createElement("DIV");
+          select_div.setAttribute("class", (select_element.disabled?"custom-select__selected custom-select__selected--disabled":"custom-select__selected"));
+          select_element.selectedIndex = select_element.selectedIndex > 0?select_element.selectedIndex:0;
 
-    /* For each element, create a new DIV that will contain the option list */
-    b = document.createElement("DIV");
-    b.setAttribute("class", "custom-select__items custom-select__items--hidden");
-    for (j = 1; j < selElmnt.length; j++) {
-      /* For each option in the original select element,
-      create a new DIV that will act as an option item */
-      c = document.createElement("DIV");
-      c.innerHTML = selElmnt.options[j].innerHTML;
+          select_div.innerHTML = select_element.options[select_element.selectedIndex].innerHTML;
+          select_div_container[i].appendChild(select_div);
 
-      c.addEventListener("click", function(e) {
-          /* When an item is clicked, update the original select box,
-          and the selected item */
-          var y, i, k, s, h;
-          s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-          h = this.parentNode.previousSibling;
+          /* For each element, create a new DIV that will contain the option list */
+          options_div = document.createElement("DIV");
+          options_div.setAttribute("class", "custom-select__items custom-select__items--hidden");
+          
+          for (j = 1; j < select_element.length; j++) {
+            /* For each option in the original select element,
+            create a new DIV that will act as an option item */
+            option_div = document.createElement("DIV");
+            option_div.innerHTML = select_element.options[j].innerHTML;
 
-          for (i = 0; i < s.length; i++) {
-            if (s.options[i].innerHTML == this.innerHTML) {
-              s.selectedIndex = i;
-              h.innerHTML = this.innerHTML;
-              y = this.parentNode.getElementsByClassName("custom-select__items--selected");
+            option_div.addEventListener("click", function(e) {
+                /* When an item is clicked, update the original select box,
+                and the selected item */
+                var y, i, k, s, h;
+                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+                h = this.parentNode.previousSibling;
 
-              for (k = 0; k < y.length; k++) {
-                y[k].removeAttribute("class");
-              }
+                for (i = 0; i < s.length; i++) {
+                  if (s.options[i].innerHTML == this.innerHTML) {
+                    s.selectedIndex = i;
+                    h.innerHTML = this.innerHTML;
+                    y = this.parentNode.getElementsByClassName("custom-select__items--selected");
 
-              this.setAttribute("class", "custom-select__items--selected");
-              break;
-            }
+                    for (k = 0; k < y.length; k++) {
+                      y[k].removeAttribute("class");
+                    }
+
+                    this.setAttribute("class", "custom-select__items--selected");
+                    break;
+                  }
+                }
+
+                h.click();
+            });
+
+            options_div.appendChild(option_div);
           }
-          h.click();
-      });
-      b.appendChild(c);
-    }
 
-    x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
-      /* When the select box is clicked, close any other select boxes,
-      and open/close the current select box if not disabled */
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("custom-select__items--hidden");
-      this.classList.toggle("custom-select__selected--active");
-    });
+          select_div_container[i].appendChild(options_div);
+          select_div.addEventListener("click", function(e) {
+            /* When the select box is clicked, close any other select boxes,
+            and open/close the current select box if not disabled */
+            e.stopPropagation();
+            closeAllSelect(this);
+            this.nextSibling.classList.toggle("custom-select__items--hidden");
+            this.classList.toggle("custom-select__selected--active");
+          });
+        }
+      }
+
+      function closeAllSelect(elmnt) 
+      {
+        /* A function that will close all select boxes in the document,
+        except the current select box */
+        var options_div, option_selected, i, arrNo = [];
+        options_div = document.getElementsByClassName("custom-select__items");
+        option_selected = document.getElementsByClassName("custom-select__selected");
+
+        for (i = 0; i < option_selected.length; i++) {
+          if (elmnt == option_selected[i]) {
+            arrNo.push(i)
+          } else {
+            option_selected[i].classList.remove("custom-select__selected--active");
+          }
+        }
+
+        for (i = 0; i < options_div.length; i++) {
+          if (arrNo.indexOf(i)) {
+            options_div[i].classList.add("custom-select__items--hidden");
+          }
+        }
+      }
+
+      /* If the user clicks anywhere outside the select box,
+      then close all select boxes: */
+      document.addEventListener("click", closeAllSelect);
+    }, 1000);
   }
 }
-
-function closeAllSelect(elmnt) 
-{
-  /* A function that will close all select boxes in the document,
-  except the current select box */
-  var x, y, i, arrNo = [];
-  x = document.getElementsByClassName("custom-select__items");
-  y = document.getElementsByClassName("custom-select__selected");
-  for (i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("custom-select__selected--active");
-    }
-  }
-  for (i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("custom-select__items--hidden");
-    }
-  }
-}
-
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
