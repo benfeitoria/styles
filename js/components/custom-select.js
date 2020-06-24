77647cf29588fc4b5dbc6284aa0ac2e7d3d2a276
Felipe Fontana <felipefontanaf@gmail.com>
2020-06-24 19:44:45 -0400

    refactor files

--- js/components/custom-select.js
+++ js/components/custom-select.js
@@ -1,99 +1,130 @@
 document.onreadystatechange = function () {
   if (document.readyState === 'complete') {
-    setTimeout(function(){
-      var select_div_container, i, j, select_element, select_div, options_div, option_div;
+    setTimeout(function () {
+      let i
+      let j
+      let selectElement
+      let selectDiv
+      let optionsDiv
+      let optionDiv
 
       /* Look for any elements with the class "custom-select" */
-      select_div_container = document.getElementsByClassName("custom-select");
+      const selectDivContainer = document.getElementsByClassName(
+        'custom-select'
+      )
 
-      for (i = 0; i < select_div_container.length; i++) {
-        if(select_div_container[i].getElementsByTagName("select")){
-          select_element = select_div_container[i].getElementsByTagName("select")[0];
-  
-          /* For each element, create a new DIV that will act as the selected item */
-          select_div = document.createElement("DIV");
-          select_div.setAttribute("class", (select_element.disabled?"custom-select__selected custom-select__selected--disabled":"custom-select__selected"));
-          select_element.selectedIndex = select_element.selectedIndex > 0?select_element.selectedIndex:0;
+      for (i = 0; i < selectDivContainer.length; i++) {
+        if (selectDivContainer[i].getElementsByTagName('select')) {
+          ;[selectElement] = selectDivContainer[i].getElementsByTagName(
+            'select'
+          )
 
-          select_div.innerHTML = select_element.options[select_element.selectedIndex].innerHTML;
-          select_div_container[i].appendChild(select_div);
+          /* For each element create a new DIV that will act as the selected item */
+          selectDiv = document.createElement('DIV')
+          selectDiv.setAttribute(
+            'class',
+            selectElement.disabled
+              ? 'custom-select__selected custom-select__selected--disabled'
+              : 'custom-select__selected'
+          )
+          selectElement.selectedIndex =
+            selectElement.selectedIndex > 0 ? selectElement.selectedIndex : 0
+
+          selectDiv.innerHTML =
+            selectElement.options[selectElement.selectedIndex].innerHTML
+          selectDivContainer[i].appendChild(selectDiv)
 
           /* For each element, create a new DIV that will contain the option list */
-          options_div = document.createElement("DIV");
-          options_div.setAttribute("class", "custom-select__items custom-select__items--hidden");
-          
-          for (j = 1; j < select_element.length; j++) {
+          optionsDiv = document.createElement('DIV')
+          optionsDiv.setAttribute(
+            'class',
+            'custom-select__items custom-select__items--hidden'
+          )
+
+          for (j = 1; j < selectElement.length; j++) {
             /* For each option in the original select element,
             create a new DIV that will act as an option item */
-            option_div = document.createElement("DIV");
-            option_div.innerHTML = select_element.options[j].innerHTML;
+            optionDiv = document.createElement('DIV')
+            optionDiv.innerHTML = selectElement.options[j].innerHTML
 
-            option_div.addEventListener("click", function(e) {
-                /* When an item is clicked, update the original select box,
+            optionDiv.addEventListener('click', function (e) {
+              /* When an item is clicked, update the original select box,
                 and the selected item */
-                var y, i, k, s, h;
-                s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-                h = this.parentNode.previousSibling;
+              let y
+              let i
+              let k
+              const [s] = this.parentNode.parentNode.getElementsByTagName(
+                'select'
+              )
 
-                for (i = 0; i < s.length; i++) {
-                  if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("custom-select__items--selected");
+              const h = this.parentNode.previousSibling
 
-                    for (k = 0; k < y.length; k++) {
-                      y[k].removeAttribute("class");
-                    }
+              for (i = 0; i < s.length; i++) {
+                if (s.options[i].innerHTML == this.innerHTML) {
+                  s.selectedIndex = i
+                  h.innerHTML = this.innerHTML
+                  y = this.parentNode.getElementsByClassName(
+                    'custom-select__items--selected'
+                  )
 
-                    this.setAttribute("class", "custom-select__items--selected");
-                    break;
+                  for (k = 0; k < y.length; k++) {
+                    y[k].removeAttribute('class')
                   }
+
+                  this.setAttribute('class', 'custom-select__items--selected')
+                  break
                 }
+              }
 
-                h.click();
-            });
+              h.click()
+            })
 
-            options_div.appendChild(option_div);
+            optionsDiv.appendChild(optionDiv)
           }
 
-          select_div_container[i].appendChild(options_div);
-          select_div.addEventListener("click", function(e) {
+          selectDivContainer[i].appendChild(optionsDiv)
+          selectDiv.addEventListener('click', function (e) {
             /* When the select box is clicked, close any other select boxes,
             and open/close the current select box if not disabled */
-            e.stopPropagation();
-            closeAllSelect(this);
-            this.nextSibling.classList.toggle("custom-select__items--hidden");
-            this.classList.toggle("custom-select__selected--active");
-          });
+            e.stopPropagation()
+            closeAllSelect(this)
+            this.nextSibling.classList.toggle('custom-select__items--hidden')
+            this.classList.toggle('custom-select__selected--active')
+          })
         }
       }
 
-      function closeAllSelect(elmnt) 
-      {
-        /* A function that will close all select boxes in the document,
-        except the current select box */
-        var options_div, option_selected, i, arrNo = [];
-        options_div = document.getElementsByClassName("custom-select__items");
-        option_selected = document.getElementsByClassName("custom-select__selected");
+      function closeAllSelect(elmnt) {
+        let i
+        const arrNo = []
 
-        for (i = 0; i < option_selected.length; i++) {
-          if (elmnt == option_selected[i]) {
+        const optionsDiv = document.getElementsByClassName(
+          'custom-select__items'
+        )
+        const optionSelected = document.getElementsByClassName(
+          'custom-select__selected'
+        )
+
+        for (i = 0; i < optionSelected.length; i++) {
+          if (elmnt == optionSelected[i]) {
             arrNo.push(i)
           } else {
-            option_selected[i].classList.remove("custom-select__selected--active");
+            optionSelected[i].classList.remove(
+              'custom-select__selected--active'
+            )
           }
         }
 
-        for (i = 0; i < options_div.length; i++) {
+        for (i = 0; i < optionsDiv.length; i++) {
           if (arrNo.indexOf(i)) {
-            options_div[i].classList.add("custom-select__items--hidden");
+            optionsDiv[i].classList.add('custom-select__items--hidden')
           }
         }
       }
 
       /* If the user clicks anywhere outside the select box,
       then close all select boxes: */
-      document.addEventListener("click", closeAllSelect);
-    }, 1000);
+      document.addEventListener('click', closeAllSelect)
+    }, 1000)
   }
 }
--- js/components/progressbar.js
+++ js/components/progressbar.js
@@ -1,14 +1,29 @@
 // Set default value r for circle in svg
-let progressBars = document.querySelectorAll('.progress-bar');
-for(let progressBar of progressBars){
-    if(progressBar.classList.contains("progress-bar--sm") && progressBar.querySelector('circle')){
-        progressBar.querySelector('circle').r.baseVal.value = "9";
-    } else if(progressBar.classList.contains("progress-bar--md") && progressBar.querySelector('circle')){
-        progressBar.querySelector('circle').r.baseVal.value = "11";
-    } else if(progressBar.classList.contains("progress-bar") && progressBar.querySelector('circle')){
-        progressBar.querySelector('circle').r.baseVal.value = "24";
-    }
-}
+const progressBars = document.querySelectorAll('.progress-bar')
+
+progressBars.forEach((progressBar) => {
+  const circle = progressBar.querySelector('circle').r.baseVal
+  if (
+    progressBar.classList.contains('progress-bar--sm') &&
+    progressBar.querySelector('circle')
+  ) {
+    circle.value = '9'
+  }
+
+  if (
+    progressBar.classList.contains('progress-bar--md') &&
+    progressBar.querySelector('circle')
+  ) {
+    circle.value = '11'
+  }
+
+  if (
+    progressBar.classList.contains('progress-bar') &&
+    progressBar.querySelector('circle')
+  ) {
+    circle.value = '24'
+  }
+})
 
 /**
  * Progress bar function
@@ -16,51 +31,43 @@ for(let progressBar of progressBars){
  * INT percent = percent value
  * STRING id = id of element
  * STRING type = type of progress bar ('circle' or 'horizontal')
-*/
-window.progressBar = function (params)
-{
-    if(!params.id) return false;
+ */
+window.progressBar = (params) => {
+  if (!params.id) return false
 
-    var elementId = "#"+params.id;
-    var percent = params.percent?params.percent:0;
-    var type = params.type?params.type:'circle';
+  const elementId = `# + ${params.id}`
+  const percent = params.percent ? params.percent : 0
+  const type = params.type ? params.type : 'circle'
 
-    if(type == 'circle' && document.querySelector(elementId)) {
-        // Circle attributes
-        var text = document.querySelector(elementId).querySelector('.progress-bar__text');
-        var circle = document.querySelector(elementId).querySelector('circle');
-        var radius = circle.r.baseVal.value;
-        var circumference = radius * 2 * Math.PI;
+  if (type !== 'circle' && document.querySelector(elementId)) {
+    // Set percent of horizontal progress bar
+    document.querySelector(elementId).value = percent
+  }
 
-        // Defines the default size of green and gray circle around the icon
-        circle.style.strokeDasharray = `${circumference} ${circumference}`;
-        circle.style.strokeDashoffset = `${circumference}`;
+  const text = document
+    .querySelector(elementId)
+    .querySelector('.progress-bar__text')
+  const circle = document.querySelector(elementId).querySelector('circle')
+  const radius = circle.r.baseVal.value
+  const circumference = radius * 2 * Math.PI
 
-        // Calculate the green circle size according to the percentage entered
-        const offset = circumference - percent / 100 * circumference;
-        circle.style.strokeDashoffset = offset;
+  // Defines the default size of green and gray circle around the icon
+  circle.style.strokeDasharray = `${circumference} ${circumference}`
+  circle.style.strokeDashoffset = `${circumference}`
 
-        // Change percentage text
-        if(text) {
-            text.innerHTML = percent+"%";
-        }
+  // Calculate the green circle size according to the percentage entered
+  const offset = circumference - (percent / 100) * circumference
+  circle.style.strokeDashoffset = offset
 
-        // Change element color when percent is 100
-        if(percent == 100) {
-            document
-                .querySelector(elementId)
-                .classList
-                .add("progress-bar--done");
-        } else {
-            document
-                .querySelector(elementId)
-                .classList
-                .remove("progress-bar--done");
-        }
-    } else if(document.querySelector(elementId)) {
-        // Set percent of horizontal progress bar
-        document
-            .querySelector(elementId)
-            .value = percent;
-    }
+  // Change percentage text
+  if (text) {
+    text.innerHTML = `${percent}%`
+  }
+
+  // Change element color when percent is 100
+  if (percent === 100) {
+    document.querySelector(elementId).classList.add('progress-bar--done')
+  } else {
+    document.querySelector(elementId).classList.remove('progress-bar--done')
+  }
 }
