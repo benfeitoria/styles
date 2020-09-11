96cdca39a32008b0c0287e709f69415e3c738654
lucas-cn <lucascorreia177@yahoo.com.br>
2020-09-11 16:33:57 -0300

    fix conditional

--- js/components/radio.js
+++ js/components/radio.js
@@ -2,16 +2,18 @@ const inputRadios = document.querySelectorAll('input[type=radio]')
 
 inputRadios.forEach((inputRadio) => {
   const element = inputRadio
-  if (element.checked) {
-    element.closest('label.radio').closest('li').classList.add('active')
-  }
+  if(element.closest('label.radio').closest('li')){
+      if (element.checked) {
+        element.closest('label.radio').closest('li').classList.add('active')
+      }
 
-  element.addEventListener('change', () => {
-    if (element.checked) {
-      ;[].forEach.call(inputRadios, function (el) {
-        el.closest('label.radio').closest('li').classList.remove('active')
+      element.addEventListener('change', () => {
+        if (element.checked) {
+          ;[].forEach.call(inputRadios, function (el) {
+            el.closest('label.radio').closest('li').classList.remove('active')
+          })
+          element.closest('label.radio').closest('li').classList.add('active')
+        }
       })
-      element.closest('label.radio').closest('li').classList.add('active')
-    }
-  })
+  }
 })
