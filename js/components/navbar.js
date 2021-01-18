c1d4143d6d54e6a95f3b10d767230cacf9c3f8ea
phil <philipe.lagreca@gmail.com>
2021-01-18 17:10:11 -0300

    Navbar check element existence

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,18 +1,23 @@
-const mobileTopNavigation = document.querySelector('.navigation--top--mobile');
+const mobileTopNavigation = document.querySelector('.navigation--top--mobile')
 const dropdownItems = mobileTopNavigation.querySelectorAll('li.dropdown')
 dropdownItems.forEach((dropdownItem) => {
-    dropdownItem.querySelector('a').addEventListener('click', (event) => {
-        event.preventDefault()
-        const element = (event.target.tagName === 'I') ? event.target.parentElement : event.target;
-        if (element.nextElementSibling !== null) {
-            element.nextElementSibling.classList.toggle('collapsed')
-        }
-    })
+  dropdownItem.querySelector('a').addEventListener('click', (event) => {
+    event.preventDefault()
+    const element =
+      event.target.tagName === 'I' ? event.target.parentElement : event.target
+    if (element.nextElementSibling !== null) {
+      element.nextElementSibling.classList.toggle('collapsed')
+    }
+  })
 })
-mobileTopNavigation
-    .querySelector('i.navigation--top--mobile__heading__menu-toggle')
-    .addEventListener('click', (event) => {
-        const menuToggle = event.target
-        menuToggle.innerText = (menuToggle.innerText === 'close') ? 'menu' : 'close'
-        mobileTopNavigation.classList.toggle('collapsed')
-    })
+const navigationMenuToggle = mobileTopNavigation.querySelector(
+  'i.navigation--top--mobile__heading__menu-toggle'
+)
+
+if (navigationMenuToggle) {
+  navigationMenuToggle.addEventListener('click', (event) => {
+    const menuToggle = event.target
+    menuToggle.innerText = menuToggle.innerText === 'close' ? 'menu' : 'close'
+    mobileTopNavigation.classList.toggle('collapsed')
+  })
+}
